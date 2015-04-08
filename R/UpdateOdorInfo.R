b1988f6b9806056dfef96729adf6c50c5d990fed
Dahaniel <daniel@muench.bio>
2015-04-08 15:00:51 +0200

    added UpdateOdorInfo.R

---
+++ R/UpdateOdorInfo.R
@@ -0,0 +1,21 @@
+#' Update the DoOR odor data withinfo from 'odor'
+#'
+#' @author Daniel Münch, \email{daniel@muench.bio}
+#'
+#'
+UpdateOdorInfo <- function() 
+{
+  for(i in ORs$OR)
+  {
+    tmp <- get(i, envir = .GlobalEnv)
+    match <- match(tmp$InChIKey, odor$InChIKey)
+    if (length(tmp) > 5) 
+    {
+      tmp <- cbind(odor[1:5], tmp[6:length(tmp)])
+    } else {
+      tmp <- odor[1:5]
+    }
+    assign(i, tmp, envir = .GlobalEnv)
+  }
+  data.format <- odor[1:5]
+}
