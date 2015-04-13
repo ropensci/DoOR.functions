cc006831134ad2b4191237ec34bc009e297c0890
Dahaniel <daniel@muench.bio>
2015-04-13 11:21:36 +0200

    added a function to remove a study from DoOR

---
+++ R/removeStudy.R
@@ -0,0 +1,36 @@
+#' Remove a study from DoOR
+#' 
+#' Use this function to remove a study from the DoOR database.
+#' \code{importNewData.R} uses this function when it detects an existing study
+#' during the import process (e.g. because you imported updated data).
+#' @param study a string containing the name of the study you want to remove
+#'   (e.g. 'Bruyne.2001.WT')
+#' @param receptors a vector of all the receptors to be checked. Defaults to all
+#'   receptors exidting in DoOR.
+#' @param responseRange the dataframe containing the info about the response
+#'   ranges of all studies (\code{response.range})
+#'   
+#' @seealso \code{\link{importNewData}}
+#'   
+#' @author Daniel Münch <\email{daniel.muench@@uni-konstanz.de}>
+#'   
+#' @examples
+#' removeStudy('Bruyne.2001.WT')
+#' 
+removeStudy <- function(study,
+                        receptors = default.val('ORs'),
+                        responseRange = default.val('response.range')
+) {
+  for(x in receptors[,"OR"]) {
+    data <- try(get(x), silent = TRUE)
+    if (any(colnames(data) == study)) {
+      pos <- which(colnames(data) == study)
+      data <- data[,-pos]
+      assign(x, data, envir = .GlobalEnv)
+      message(paste('removed',study,'from',x,'.'))       
+    }
+  }
+  responseRange <- responseRange[- which(responseRange$study == study),] 
+  assign("response.range", responseRange, envir = .GlobalEnv)
+  message(paste('removed',study,'from DoOR. Remember to requild the response matrices.'))
+}
