db4e4c23887b6d11029696c0cb1aa9839858c9d6
Dahaniel <daniel@muench.bio>
2015-04-21 10:44:40 +0200

    check if object is existing before reloading it

--- R/default.val.R
+++ R/default.val.R
@@ -13,17 +13,7 @@
 #' 
 #' default.val(DoOR_default = "select.MD")
 #' 
-default.val <-
-function(DoOR_default)
-
-# part of the DoOR package: (c) 2009 C. Giovanni Galizia, Daniel Muench, Martin Strauch, Anja Nissler, Shouwen Ma
-# Neurobiology, University of Konstanz, Germany
-
-## return default values for DoOR functions
-
-#  DoOR_default: character string
-
-{
+default.val <- function(DoOR_default) {
 
 	## real number
 	if (DoOR_default == "select.MDValue") 	{ return(0.1415) } # selectModel.R; modelRP.R; CreateDatabase.R; CreateDatabase.R
@@ -59,15 +49,74 @@ function(DoOR_default)
 	if (DoOR_default == "interval.X") 	{ return(c(-1,2)) } # compute_MD.R; modelfunction.R; projectPoints.R
 
 	## data frame
-	if (DoOR_default == "AL256") 		{ data(AL256); return(AL256) } # ALimage.R
-	if (DoOR_default == "data.format") 	{ data(data.format); return(data.format) } # importNewData.R
-	if (DoOR_default == "odor.data") 	{ data(odor); return(odor) } # showOdor.R
-	if (DoOR_default == "odor.dist") 	{ data(odor.dist); return(odor.dist) } # ALimage.R
-	if (DoOR_default == "OGN") 		{ data(OGN); return(OGN) } # ALimage.R
-	if (DoOR_default == "ORs") 		{ data(ORs); return(ORs) } # exportData.R; LoadRD.R; loadRDlist.R; importNewData.R
-	if (DoOR_default == "response.matrix") 	{ data(response.matrix); return(response.matrix) } # exportData.R; findRespNorm.R
-	if (DoOR_default == "response.range") 	{ data(response.range); return(response.range) } # exportData.R; findResp.R; importNewData.R; updateDatabase.R; modelRP.R
-	if (DoOR_default == "unglobalNorm_response.matrix") { data(unglobalNorm_response.matrix); return(unglobalNorm_response.matrix) } # exportData.R
-	if (DoOR_default == "weight.globNorm") 	{ data(weight.globNorm); return(weight.globNorm) } # exportData.R; importNewData.R; updateDatabase.R; modelRP.R
+	if (DoOR_default == "AL256") {
+    if (!exists("AL256")) {
+      data(AL256)
+    }
+    return(AL256)
+  } # ALimage.R
+  
+	if (DoOR_default == "data.format") {
+	  if (!exists("data.format")) {
+	    data(data.format)
+	  }
+	  return(data.format)
+	} # importNewData.R
+
+	if (DoOR_default == "odor.data") {
+	  if (!exists("odor.data")) {
+	    data(odor)
+	  }
+	  return(odor)
+	} # showOdor.R
+  
+	if (DoOR_default == "odor.dist") {
+	  if (!exists("odor.dist")) {
+	    data(odor.dist)
+	  }
+	  return(odor.dist)
+	} # ALimage.R
+  
+	if (DoOR_default == "weight.globNorm") {
+	  if (!exists("weight.globNorm")) {
+	    data(weight.globNorm)
+	  }
+	  return(weight.globNorm)
+	} # exportData.R; importNewData.R; updateDatabase.R; modelRP.R
+
+	if (DoOR_default == "OGN") {
+	  if (!exists("OGN")) {
+	    data(OGN)
+	  }
+	  return(OGN)
+	} # ALimage.R
+  
+	if (DoOR_default == "ORs") {
+	  if (!exists("ORs")) {
+	    data(ORs)
+	  }
+	  return(ORs)
+	} # exportData.R; LoadRD.R; loadRDlist.R; importNewData.R
+  
+	if (DoOR_default == "response.matrix") {
+	  if (!exists("response.matrix")) {
+	    data(response.matrix)
+	  }
+	  return(response.matrix)
+	} # exportData.R; findRespNorm.R
+  
+	if (DoOR_default == "response.range") {
+	  if (!exists("response.range")) {
+	    data(response.range)
+	  }
+	  return(response.range)
+	} # exportData.R; findResp.R; importNewData.R; updateDatabase.R; modelRP.R
+  
+	if (DoOR_default == "unglobalNorm_response.matrix") {
+	  if (!exists("unglobalNorm_response.matrix")) {
+	    data(unglobalNorm_response.matrix)
+	  }
+	  return(unglobalNorm_response.matrix)
+	} # exportData.R
 
 }
