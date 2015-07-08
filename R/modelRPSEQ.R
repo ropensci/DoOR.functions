f6e4965b5ebceb166adcfabe7e6499337d67ce08
Dahaniel <daniel@muench.bio>
2015-07-08 15:42:53 +0200

    code cosmetics

--- R/modelRPSEQ.R
+++ R/modelRPSEQ.R
@@ -34,42 +34,45 @@
 #' selected.merg <- modelRPSEQ(Or35a, SEQ = SEQ)
 #' 
 modelRPSEQ <-
-function(data, SEQ, overlapValues = default.val("overlapValues"), plot=F) {
-  nv          <- as.numeric( c( (default.val("num.charColumns")+1):dim(data)[2] ) ) # positions of columns that contain odor response vectors
-  name.stud   <- names(data)[nv]
-  pda         <- apply(as.data.frame(data[, nv]), 2, DoORnorm) # processing data
-  mseq        <- match(SEQ, name.stud) # match given sequence to the column names of response data
-  first.study <- SEQ[1] 
-  rest.study  <- SEQ[-match(first.study,SEQ)]
-  
-  if (plot == TRUE)  {
-    lenNV_1  <- length(nv) - 1
-    nframe.X <- ceiling(lenNV_1^0.5)
-    nframe.Y <- ceiling(lenNV_1/nframe.X)
-    op       <- par(mfrow = c((nframe.Y), (nframe.X)))
-  }
-  
-  # start merging following the given sequence
-  y <- pda[,first.study]
-  ylab <- first.study
-  for (i in rest.study) {
-    x 	  <- pda[,i]
-    if (dim(na.omit(cbind(x,y)))[1] < overlapValues)
-      stop(paste("less than", overlapValues, "observations between two datasets"))
+  function(data, 
+           SEQ, 
+           overlapValues = default.val("overlapValues"), 
+           plot=F) {
+    nv          <- as.numeric( c( (default.val("num.charColumns")+1):dim(data)[2] ) ) # positions of columns that contain odor response vectors
+    name.stud   <- names(data)[nv]
+    pda         <- apply(as.data.frame(data[, nv]), 2, DoORnorm) # processing data
+    mseq        <- match(SEQ, name.stud) # match given sequence to the column names of response data
+    first.study <- SEQ[1] 
+    rest.study  <- SEQ[-match(first.study,SEQ)]
     
-    projected <- projectPoints(x, y, plot = plot, xlab = i, ylab = ylab, title = plot)
-    res 	  <- rep(NA, length = dim(pda)[1])
-    
-    # the output of projectPoints is a list with odor responses, either observed in both studies, or only in one study.
-    res[projected$Double.Observations$ID] <- projected$Double.Observations$NDR
-    if (is.data.frame(projected$Single.Observation)) {
-      res[projected$Single.Observation$ID] <- projected$Single.Observation$NDR
+    if (plot == TRUE)  {
+      lenNV_1  <- length(nv) - 1
+      nframe.X <- ceiling(lenNV_1^0.5)
+      nframe.Y <- ceiling(lenNV_1/nframe.X)
+      op       <- par(mfrow = c((nframe.Y), (nframe.X)))
     }
     
-    y <- res
-    ylab <- "merged_data"
+    # start merging following the given sequence
+    y <- pda[,first.study]
+    ylab <- first.study
+    for (i in rest.study) {
+      x 	  <- pda[,i]
+      if (dim(na.omit(cbind(x,y)))[1] < overlapValues)
+        stop(paste("less than", overlapValues, "observations between two datasets"))
+      
+      projected <- projectPoints(x, y, plot = plot, xlab = i, ylab = ylab, title = plot)
+      res 	  <- rep(NA, length = dim(pda)[1])
+      
+      # the output of projectPoints is a list with odor responses, either observed in both studies, or only in one study.
+      res[projected$Double.Observations$ID] <- projected$Double.Observations$NDR
+      if (is.data.frame(projected$Single.Observation)) {
+        res[projected$Single.Observation$ID] <- projected$Single.Observation$NDR
+      }
+      
+      y <- res
+      ylab <- "merged_data"
+      
+    } # END merging
     
-  } # END merging
-  
-  return(y)
-}
+    return(y)
+  }
