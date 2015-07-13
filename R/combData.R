0451b4418195895f4ed209c9ba1d061f32e2428c
Dahaniel <daniel@muench.bio>
2015-07-13 19:48:43 +0200

    code cosmetics

--- R/combData.R
+++ R/combData.R
@@ -14,6 +14,7 @@
 #' column will be combined into data1.
 #' @param assigned.name charater string; assigning a name to combined column.
 #' @author Shouwen Ma <\email{shouwen.ma@@uni-konstanz.de}>
+#' @author Daniel Münch <\email{daniel.muench@@uni-konstanz.de}>
 #' @keywords data
 #' @examples
 #' 
@@ -26,47 +27,44 @@
 #' 
 combData <- function(data1, data2, by.data2, assigned.name, ident = default.val("ident")) 
 {
-	if(any(duplicated(data2[,ident]))) stop('There are duplicated identifiers in the new dataset, please solve this first.')
+  if(any(duplicated(data2[,ident]))) stop('There are duplicated identifiers in the new dataset, please solve this first.')
   new.odor <- which(is.na(match(data2[,ident],data1[,ident])))
-
-	# if no new odor
-	if (is.na(new.odor[1])) 
-  {
-		output 	  <- data1
-		matchOdor <- match(data2[,ident], output[,ident])
-		if (missing(assigned.name)) { assigned.name <- by.data2 }
-		output[matchOdor, assigned.name] <- data2[, by.data2]
-	}
-	else 
-  {
-		data_NewOdor <- data2[new.odor,]
-		l.d1 <- length(data1[,ident])
-		l.d2 <- length(data_NewOdor[,ident])
-		nrow.new <- l.d1 + l.d2
-		first_NewOdor <- data1[(l.d1+1),]
-		output <- data1
-	
+  
+  # if no new odor
+  if (is.na(new.odor[1])) {
+    output 	  <- data1
+    matchOdor <- match(data2[,ident], output[,ident])
+    if (missing(assigned.name)) 
+      assigned.name <- by.data2
+    output[matchOdor, assigned.name] <- data2[, by.data2]
+  } else {
+    data_NewOdor <- data2[new.odor,]
+    l.d1 <- length(data1[,ident])
+    l.d2 <- length(data_NewOdor[,ident])
+    nrow.new <- l.d1 + l.d2
+    first_NewOdor <- data1[(l.d1+1),]
+    output <- data1
+    
     # add new odors to the frame one by one
-		for (i in seq(l.d2)) 
-    {
-			next_NewOdor 	     <- first_NewOdor
-			next_NewOdor[,ident] <- as.character(data_NewOdor[i,ident])
-			output 		     <- rbind(output,next_NewOdor)
-			rownames(output)     <- seq(l.d1+i)
-	  }
-
-		if (missing(assigned.name)) { assigned.name <- by.data2 }
-	
+    for (i in seq(l.d2)) {
+      next_NewOdor 	     <- first_NewOdor
+      next_NewOdor[,ident] <- as.character(data_NewOdor[i,ident])
+      output 		     <- rbind(output,next_NewOdor)
+      rownames(output)     <- seq(l.d1+i)
+    }
+    
+    if (missing(assigned.name)) 
+      assigned.name <- by.data2
+    
     # add response values of new odors to the output one by one
-		for (i in seq(l.d2)) 
-		{
-			output[l.d1+i,assigned.name] <- data_NewOdor[i,by.data2]
-		}
-	
+    for (i in seq(l.d2)) {
+      output[l.d1+i,assigned.name] <- data_NewOdor[i,by.data2]
+    }
+    
     matchOdor <- match(data2[,ident],output[,ident])
-		output[matchOdor, assigned.name] <- data2[, by.data2]
-	}
-
-
-return(output)
+    output[matchOdor, assigned.name] <- data2[, by.data2]
+  }
+  
+  
+  return(output)
 }
--- R/globalNorm.R
+++ R/globalNorm.R
@@ -25,6 +25,7 @@
 #' measured in each study.
 #' @author Shouwen Ma <\email{shouwen.ma@@uni-konstanz.de}>
 #' @keywords math
+#' @export
 #' @examples
 #' 
 #' 
@@ -39,35 +40,12 @@
 #' GN_values_ORx <- globalNorm(RMAX, SMAX, MV = mergedValues_ORx, name.Stud, weightGlobNorm = weight.globNorm, responseRange = response.range)
 #' 
 globalNorm <-
-function(RMAX, SMAX, MV, name.Stud, weightGlobNorm = default.val("weight.globNorm"), responseRange = default.val("response.range"))  
-
-
-# part of the DoOR package: (c) 2009 C. Giovanni Galizia, Daniel Muench, Martin Strauch, Anja Nissler, Shouwen Ma
-# Neurobiology, University of Konstanz, Germany
-
-## normalize the merged data globally using weight 
-
-# 	Some data contribute more than others based on how many receptors and odors were measured, 
-#	so that we introduce a weighted mean to scale the model response values. 
-#	If all weights are equal, then the weighted mean is the same as the arithmetic mean.
-
-# mathematical expression for weighted scale:  ( sum(weight_receptors[i]*(RMAX[i]/SMAX[i])) + sum(weight_odors[i]*(RMAX[i]/SMAX[i])) ) / ( sum(weight_receptors[i]) + sum(weight_odors[i]) ) 
-
-#  RMAX: 		numeric vector; a vector across all studies of maximum responses for this receptor.
-#  SMAX: 		numeric vector; a vector across all studies of maximum response in all receptors available in each study.
-#  MV  : 		numeric vector; a vector of model response values to be normalized (for this receptor).
-#  name.Stud: 		character vector; names of involved studies.
-#  weightGlobNorm: 	data matrix; a receptor-by-study matrix; value 1 indicates available measurement in this study for this receptor; value NA indicates NOT.
-#  responseRange : 	data frame; containing the information about the response range of each study and how many odors were measured in each study.
-
-##  output is the normalized response vector (dimension as MV)
-
-{
-	weight.frame 	 <- data.frame(weightGlobNorm[,name.Stud])  			  # take the data subset for weight that involves only given studies
-	weight_receptors <- apply(weight.frame,2,function(x) length(which(x==1))) # how many tested receptors in given studies
-	match_stud 	 <- match(name.Stud, responseRange[,"study"])
-	weight_odors 	 <- responseRange[match_stud, "n_odors"]
-	scale 		 <- ( sum(weight_receptors*(RMAX/SMAX),na.rm=TRUE) + sum(weight_odors*(RMAX/SMAX),na.rm=TRUE) )/(sum(weight_receptors,na.rm=TRUE)+ sum(weight_odors,na.rm=TRUE))
-	globalNorm 	 <- scale * MV
-	return(globalNorm)
-}
+  function(RMAX, SMAX, MV, name.Stud, weightGlobNorm = default.val("weight.globNorm"), responseRange = default.val("response.range")) {
+    weight.frame 	 <- data.frame(weightGlobNorm[,name.Stud])  			  # take the data subset for weight that involves only given studies
+    weight_receptors <- apply(weight.frame,2,function(x) length(which(x==1))) # how many tested receptors in given studies
+    match_stud 	 <- match(name.Stud, responseRange[,"study"])
+    weight_odors 	 <- responseRange[match_stud, "n_odors"]
+    scale 		 <- ( sum(weight_receptors*(RMAX/SMAX),na.rm=TRUE) + sum(weight_odors*(RMAX/SMAX),na.rm=TRUE) )/(sum(weight_receptors,na.rm=TRUE)+ sum(weight_odors,na.rm=TRUE))
+    globalNorm 	 <- scale * MV
+    return(globalNorm)
+  }
