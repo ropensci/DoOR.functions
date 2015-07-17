db3ede3e0c27e5750094a4ba358b07a902ca4e0b
Daniel Münch <daniel@muench.bio>
2015-07-17 11:12:14 +0200

    works on full DoOR response matrices now

--- R/resetSFR.R
+++ R/resetSFR.R
@@ -4,20 +4,27 @@
 #' 
 #' Performs a simple subtraction of the SFR value.
 #' 
-#' @param x a numeric vector of input values.
-#' @param sfr a numeric parameter, indicating spontaneous firing rate value.
+#' @param x numeric or DoOR response matrix; input values
+#' @param sfr numeric or character; either a value to subtract if x is a vector or an InChIKey if x is a DoOR response matrix
 #' @author Daniel Münch <\email{daniel.muench@@uni-konstanz.de}>
-#' @keywords math
 #' @export
 #' @examples
 #' 
 #' library(DoOR.data)
 #' data(response.matrix)
-#' response.matrix.SFRreset <- apply(response.matrix, 2, function(x) resetSFR(x,x['SFR']))
+#' response.matrix.SFRreset <- resetSFR(response.matrix, "SFR")
 #' 
 resetSFR <- function(x, sfr) {
-	if (is.na(sfr)) sfr <- 0
-  if (all(is.na(x))) return(x)
-  rS <- (x - sfr)
-	return(rS)
+  if(is.numeric(x)) {
+    if(!is.numeric(sfr))
+      stop("As x is a vector, sfr has to be a numeric value to subtract!")
+    if (is.na(sfr)) sfr <- 0
+    if (all(is.na(x))) return(x)
+    reset <- (x - sfr)
+  } else {
+    if(!(is.character(sfr) | is.factor(sfr)))
+      stop("As x is a full DoOR response matrix, sfr has to be the InChIKey of the odorant that should be subtracted")
+    reset   <- apply(x, 2, function(y) resetSFR(y,y[sfr]))
+  }
+  return(reset)
 }
