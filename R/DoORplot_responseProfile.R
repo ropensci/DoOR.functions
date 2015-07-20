04b238ed05304dbca5135bda96b3238820e82346
Dahaniel <daniel@muench.bio>
2015-07-20 21:56:06 +0200

    added control for base_size and set default responseMatrix and possibility to subtract SFR

--- R/DoORplot_responseProfile.R
+++ R/DoORplot_responseProfile.R
@@ -2,13 +2,15 @@
 #' 
 #' create a barplot of a DoOR response profile
 #'
-#' @param data a DoOR response.matrix
-#' @param receptor character string, receptor name, any of colnames(response.matrix)
-#' @param tag chemical identifier for annotation
-#' @param colored color code the bars according to the response value?
-#' @param colors a vector of 5 colors (2 for values < 0, 1 value for 0 and 3 values > 0)
-#' @param limits the limits for the colorscale and the x axis, global range of data will be used if empty
-#' @param scalebar add or suppress scalebars
+#' @param responseMatrix a DoOR response.matrix
+#' @param receptor character; receptor name, any of colnames(response.matrix)
+#' @param tag character; chemical identifier for annotation
+#' @param colored logical; color code the bars according to the response value?
+#' @param colors character vector; a vector of 5 colors (2 for values < 0, 1 value for 0 and 3 values > 0)
+#' @param limits numeric of length 2; the limits for the colorscale and the x axis, global range of data will be used if empty
+#' @param scalebar logical; add or suppress scalebars
+#' @param base_size numeric; the base font size for the ggplot2 plot
+#' @param zero character; the odorant response that is set to 0, defaults to "SFR"
 #'
 #' @return ggplot2 plot
 #' @export
@@ -16,32 +18,43 @@
 #' @examples 
 #' DoORplot_responseProfile(response.matrix, "Or22a")
 #' DoORplot_responseProfile(response.matrix, "Or22a", tag = "Name")
-DoORplot_responseProfile <- function(data,
-                                     receptor,
+DoORplot_responseProfile <- function(receptor,
+                                     responseMatrix = default.val("response.matrix"),
                                      tag = default.val("tag"),
                                      colored = T,
                                      colors = default.val("colors"),
                                      limits,
-                                     scalebar = default.val("scalebar")
+                                     zero = default.val("zero"),
+                                     scalebar = default.val("scalebar"), 
+                                     base_size = 12
 ) {
+  if(zero != "")
+    responseMatrix <- resetSFR(responseMatrix, zero)
   
-  if(missing(limits) & colored == T)
-    limits <- range(data, na.rm=T)
+  if(missing(limits))
+    limits <- range(responseMatrix, na.rm=T)
   
-  data <- na.omit(data.frame(odorant = rownames(data), value = data[,receptor]))
+  if(limits[1] < 0) {
+    values <- DoORnorm(c(limits[1], limits[1]/2, 0, limits[2]/3, limits[2]/1.5, limits[2]))
+  } else {
+    values <- DoORnorm(c(0, limits[2]/3, limits[2]/1.5, limits[2]))
+    colors <- colors[3:6]
+  }
+  
+  responseMatrix <- na.omit(data.frame(odorant = rownames(responseMatrix), value = responseMatrix[,receptor]))
   if(tag != "InChIKey") 
-    data$odorant <- odor[match(data$odorant, odor$InChIKey),tag]
-  data <- data[order(data$value),]
-  data$odorant <- factor(data$odorant, levels = data$odorant)
+    responseMatrix$odorant <- odor[match(responseMatrix$odorant, odor$InChIKey),tag]
+  responseMatrix <- responseMatrix[order(responseMatrix$value),]
+  responseMatrix$odorant <- factor(responseMatrix$odorant, levels = responseMatrix$odorant)
   
-  plot <- ggplot2::ggplot(data, ggplot2::aes(x = odorant, y = value)) + 
-    ggplot2::theme_minimal() +
+  plot <- ggplot2::ggplot(responseMatrix, ggplot2::aes(x = odorant, y = value)) + 
+    ggplot2::theme_minimal(base_size = base_size) +
     ggplot2::coord_flip(ylim = limits) +
     ggplot2::ggtitle(receptor)
   
   if(colored == T) {
-    plot <- plot + ggplot2::geom_bar(ggplot2::aes(fill = value), stat = "identity", position = "identity", color = "lightgrey") +
-      ggplot2::scale_fill_gradientn(colours = colors, space = "rgb", values = DoORnorm(c(limits[1], limits[1]/2, 0, limits[2]/3, limits[2]/1.5, limits[2])),limits=limits)
+    plot <- plot + ggplot2::geom_bar(ggplot2::aes(fill = value), stat = "identity", position = "identity", color = "grey") +
+      ggplot2::scale_fill_gradientn(colours = colors, space = "rgb", values = values, limits = limits)
   } else {
     plot <- plot + ggplot2::geom_bar(stat = "identity", position = "identity", width = .9)
   }
