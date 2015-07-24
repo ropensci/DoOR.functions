ed44f2929449bcf0bfd91e391e5715214dda5bfd
Dahaniel <daniel@muench.bio>
2015-07-24 16:05:41 +0200

    basic code cosmetics

--- R/DoOREst.R
+++ R/DoOREst.R
@@ -2,11 +2,11 @@
 #' 
 #' Estimate the missing entries in a response data
 #' 
-#' A warp programe using Pearson Correlation or k-nearest neighbors
-#' estimate the missing entries in a response data.
+#' A wrapper programe for using Pearson Correlation or k-nearest neighbors to
+#' estimate the missing entries in a response matrix.
 #' 
-#' @param da a data frame or matrix; contaning the consensus response values.
-#' @param nodor a numeric value; specifying the number of the selected odors.
+#' @param data a data frame or matrix; contaning the consensus response values
+#' @param nodor a numeric value; specifying the number of the selected odors
 #' @param method character string; specifying the method ("PC" (Pearson's
 #' coefficient) and "Knn" (k nearest neighbors)) for estimation, the default is
 #' "PC".
@@ -17,69 +17,51 @@
 #' @export
 #' @keywords math
 #' @examples
-#' 
+#' \dontrun{
 #' library(DoOR.data)
 #' data(response.matrix)
-#' # est_data <- DoOREst(da=response.matrix, nodor = 3)
-#' 
+#' subset <- response.matrix[1:100, 11:30]
+#' est.data <- DoOREst(data = subset, nodor = 6)
+#' }
 DoOREst <-
-function(da, nodor, method = "PC")
-
-# part of the DoOR package: (c) 2009 C. Giovanni Galizia, Daniel Muench, Martin Strauch, Anja Nissler, Shouwen Ma
-# Neurobiology, University of Konstanz, Germany
-
-# DoOREst.R :
-####################
-
-# estimate NA entries in a consensus response data
-
-# input parameters:
-###################
-
-# da 		: data frame or a matrix; a consensus response data.
-# nodor 	: numeric; number of selected odors.
-# method 	: character string; specifying the method for estimation, default is "Knn" (k nearest neighbors). The alternative is "PC" (Pearson's coefficient).
-
-## output is a consensus response data that also constains the estimated values.
-
-{
-
-	# find where is missing in the data
-	inputdata <- as.matrix(da)
-	outputdata <- inputdata
-	whichmissing <- which(is.na(inputdata),arr.ind=TRUE)
-	dim_whichmissing <- dim(whichmissing)
-	precentageMissing <- round(dim_whichmissing[1]/length(inputdata),4)*100
-	message(paste("There are",dim_whichmissing[1],paste( "(",precentageMissing,"%)",sep=""), "missing values in the given data."))
-
-	for (i in 1:dim_whichmissing[1]) {
-
-	receptor <- colnames(inputdata)[whichmissing[i,2]]
-	inchikey 	 <- rownames(inputdata)[whichmissing[i,1]]
-	
-	if (method == "Knn") {
-		tryestmatine <- try(LLSIestKnn(InChIKey = inchikey, 
-					receptor = receptor, 
-					response_matrix = inputdata, 
-					nodor=nodor),TRUE)
-	}
-
-	if (method == "PC") {
-		tryestmatine <- try(LLSIestPC(InChIKey = inchikey, 
-					receptor = receptor, 
-					response_matrix = inputdata, 
-					nodor=nodor),TRUE)
-	}
-
-	if (inherits(tryestmatine, "try-error")) { print(paste("The odorant response of",receptor, "to", inchikey, "can not be estimated.")) }
-	else { outputdata[inchikey, receptor] <- tryestmatine$estimation }
-	if (inherits(tryestmatine, "Warning messages:")) { stop }
-	}
-	whichmissingOutput 	<- which(is.na(outputdata),arr.ind=TRUE)
-	dim_whichmissingOutput 	<- dim(whichmissingOutput)
-	precentageMissingOutput <- round(dim_whichmissingOutput[1]/length(outputdata),4)*100
-	N_estimate <- dim_whichmissing[1] - dim_whichmissingOutput[1]
-	message(paste("There are",dim_whichmissingOutput[1],paste( "(",precentageMissingOutput,"%)",sep=""), "missing values in the output data."))
-	message(paste(N_estimate,"values were estimated."))
-	return(outputdata)
+  function(data, nodor, method = "PC") {
+  
+  # find where is missing in the data
+  inputdata <- as.matrix(data)
+  outputdata <- inputdata
+  whichmissing <- which(is.na(inputdata),arr.ind=TRUE)
+  dim_whichmissing <- dim(whichmissing)
+  precentageMissing <- round(dim_whichmissing[1]/length(inputdata),4)*100
+  message(paste("There are",dim_whichmissing[1],paste( "(",precentageMissing,"%)",sep=""), "missing values in the given data."))
+  
+  for (i in 1:dim_whichmissing[1]) {
+    
+    receptor <- colnames(inputdata)[whichmissing[i,2]]
+    inchikey 	 <- rownames(inputdata)[whichmissing[i,1]]
+    
+    if (method == "Knn") {
+      tryestmatine <- try(LLSIestKnn(InChIKey = inchikey, 
+                                     receptor = receptor, 
+                                     response_matrix = inputdata, 
+                                     nodor=nodor),TRUE)
+    }
+    
+    if (method == "PC") {
+      tryestmatine <- try(LLSIestPC(InChIKey = inchikey, 
+                                    receptor = receptor, 
+                                    response_matrix = inputdata, 
+                                    nodor=nodor),TRUE)
+    }
+    
+    if (inherits(tryestmatine, "try-error")) { print(paste("The odorant response of",receptor, "to", inchikey, "can not be estimated.")) }
+    else { outputdata[inchikey, receptor] <- tryestmatine$estimation }
+    if (inherits(tryestmatine, "Warning messages:")) { stop }
+  }
+  whichmissingOutput 	<- which(is.na(outputdata),arr.ind=TRUE)
+  dim_whichmissingOutput 	<- dim(whichmissingOutput)
+  precentageMissingOutput <- round(dim_whichmissingOutput[1]/length(outputdata),4)*100
+  N_estimate <- dim_whichmissing[1] - dim_whichmissingOutput[1]
+  message(paste("There are",dim_whichmissingOutput[1],paste( "(",precentageMissingOutput,"%)",sep=""), "missing values in the output data."))
+  message(paste(N_estimate,"values were estimated."))
+  return(outputdata)
 }
--- R/LLSIestKnn.R
+++ R/LLSIestKnn.R
@@ -32,99 +32,99 @@
 # LLSIestKnn(InChIKey = "MQWCXKGKQLNYQG-UHFFFAOYSA-N", receptor = "Or22a" )
 # 
 LLSIestKnn <-
-function(InChIKey, 
-		receptor, 
- 		response_matrix = default.val("response.matrix"), 
-		nodor = 3)
-{
-
-# part of the DoOR package: (c) 2009 C. Giovanni Galizia, Daniel Muench, Martin Strauch, Anja Nissler, Shouwen Ma
-# Neurobiology, University of Konstanz, Germany
-
-### subfunction
-
-
-	require (class)
-	nearest <- function (X, n, k=3)
-	## source code reference:  Hans Werner Borchers, http://tolstoy.newcastle.edu.au/R/help/04/02/0089.html
-	## Find k nearest neighbors of X[n, ] in the data frame
-	## or matrix X, utilizing function knn1 k-times.
-	{
-		N <- nrow(X)
-    		# inds contains the indices of nearest neighbors
-    		inds <- c(n); i <- 0
-    		while (i < k) 
-		{
-        	# use knn1 for one index...
-        		j <- as.integer(knn1(X [-inds, ], X[n, ], 1:(N-length(inds))))
-        		# ...and change to true index of neighbor
-        		inds <- c(inds, setdiff(1:N, inds)[j])
-        		i <- i+1
-    		}
-    		# return nearest neighbor indices (without n, of course)
-    		return(inds[-1])
-	}	 
-
-	available <- function(x) { length(which(!is.na(x))) }
-
-
-	## main program starts here
-
-
-	# first: subtract the non-NA vectors (b, w) and matrix A to complete the linear combination 
-	#	[alfa 	w]
-	#	[b    	A]
-	# where "alfa" is the unknown odorant response, "w" is a vector of odorant responses of target odor, "b" is a vector of odorant responses of target receptor, after completing "b" and "w", the matrix "A" is formed.
-
-
-	# localize the target receptor and odor in sorted response matrix
-	whereTargetReceptor 	<- match(receptor,colnames(response_matrix))
-	whereTargetodor 	<- match(InChIKey,rownames(response_matrix))
-
-	# non-NA vectors (b ("candicateOdors") and w ("candicateReceptors") ) as candicates
-	candicateReceptors 	<- which(!is.na(response_matrix[whereTargetodor,]))
-	Name_candicateReceptors <- colnames(response_matrix)[candicateReceptors]
-	candicateOdors 		<- which(!is.na(response_matrix[,whereTargetReceptor]))
-	Name_candicateOdors 	<- rownames(response_matrix)[candicateOdors]
-
-
-	# omit the columns and rows that contain NA
-
-	candi_A <- na.omit( response_matrix[candicateOdors, candicateReceptors])
-
-	# detect the posistions of selected odors and receptors
-	matchOdor <- match( rownames(candi_A),rownames(response_matrix) )
-	matchReceptor <- match( colnames(candi_A), colnames(response_matrix) )
-
-	# vector "w"
-	w <- c( as.matrix( response_matrix[InChIKey, matchReceptor] ) )
-	selectReceptor <- names(response_matrix[InChIKey, matchReceptor])
-
-	# the odors are not yet sorted.
-	unsorted_b <- c( as.matrix( response_matrix[matchOdor, receptor] ) )
-
-	## sorting odors:
-
-	if (missing(nodor)) { nodor = dim(candi_A)[1] }
-	# find neighbors
-	kNeighbors <- (nearest(X = rbind(w, candi_A), n = 1 , k = nodor) - 1)
-
-	# matrix "A"
-	A <- candi_A[kNeighbors,]
-
-	# vector "b"
-	b <- unsorted_b[kNeighbors]
-	selectedOdor <- rownames(candi_A)[kNeighbors]
-
-	# transpose matrix A
-	if (dim(A)[1] == 1) { transp_A <- t(t(c(as.matrix(A)))) } # transpose from 1 x m to m x 1 matrix
-	else { transp_A <- t(A) }
-
-	alfa <- t(b) %*% PseudoInverse(transp_A) %*% as.matrix(w)
-
-	result <- list(estimation = alfa,
-			selected.receptors = selectReceptor,
-			selected.odors = selectedOdor)
-
-	return(result)
-}
+  function(InChIKey, 
+           receptor, 
+           response_matrix = default.val("response.matrix"), 
+           nodor = 3)
+  {
+    
+    # part of the DoOR package: (c) 2009 C. Giovanni Galizia, Daniel Muench, Martin Strauch, Anja Nissler, Shouwen Ma
+    # Neurobiology, University of Konstanz, Germany
+    
+    ### subfunction
+    
+    
+    require (class)
+    nearest <- function (X, n, k=3)
+      ## source code reference:  Hans Werner Borchers, http://tolstoy.newcastle.edu.au/R/help/04/02/0089.html
+      ## Find k nearest neighbors of X[n, ] in the data frame
+      ## or matrix X, utilizing function knn1 k-times.
+    {
+      N <- nrow(X)
+      # inds contains the indices of nearest neighbors
+      inds <- c(n); i <- 0
+      while (i < k) 
+      {
+        # use knn1 for one index...
+        j <- as.integer(knn1(X [-inds, ], X[n, ], 1:(N-length(inds))))
+        # ...and change to true index of neighbor
+        inds <- c(inds, setdiff(1:N, inds)[j])
+        i <- i+1
+      }
+      # return nearest neighbor indices (without n, of course)
+      return(inds[-1])
+    }	 
+    
+    available <- function(x) { length(which(!is.na(x))) }
+    
+    
+    ## main program starts here
+    
+    
+    # first: subtract the non-NA vectors (b, w) and matrix A to complete the linear combination 
+    #	[alfa 	w]
+    #	[b    	A]
+    # where "alfa" is the unknown odorant response, "w" is a vector of odorant responses of target odor, "b" is a vector of odorant responses of target receptor, after completing "b" and "w", the matrix "A" is formed.
+    
+    
+    # localize the target receptor and odor in sorted response matrix
+    whereTargetReceptor 	<- match(receptor,colnames(response_matrix))
+    whereTargetodor 	<- match(InChIKey,rownames(response_matrix))
+    
+    # non-NA vectors (b ("candicateOdors") and w ("candicateReceptors") ) as candicates
+    candicateReceptors 	<- which(!is.na(response_matrix[whereTargetodor,]))
+    Name_candicateReceptors <- colnames(response_matrix)[candicateReceptors]
+    candicateOdors 		<- which(!is.na(response_matrix[,whereTargetReceptor]))
+    Name_candicateOdors 	<- rownames(response_matrix)[candicateOdors]
+    
+    
+    # omit the columns and rows that contain NA
+    
+    candi_A <- na.omit( response_matrix[candicateOdors, candicateReceptors])
+    
+    # detect the posistions of selected odors and receptors
+    matchOdor <- match( rownames(candi_A),rownames(response_matrix) )
+    matchReceptor <- match( colnames(candi_A), colnames(response_matrix) )
+    
+    # vector "w"
+    w <- c( as.matrix( response_matrix[InChIKey, matchReceptor] ) )
+    selectReceptor <- names(response_matrix[InChIKey, matchReceptor])
+    
+    # the odors are not yet sorted.
+    unsorted_b <- c( as.matrix( response_matrix[matchOdor, receptor] ) )
+    
+    ## sorting odors:
+    
+    if (missing(nodor)) { nodor = dim(candi_A)[1] }
+    # find neighbors
+    kNeighbors <- (nearest(X = rbind(w, candi_A), n = 1 , k = nodor) - 1)
+    
+    # matrix "A"
+    A <- candi_A[kNeighbors,]
+    
+    # vector "b"
+    b <- unsorted_b[kNeighbors]
+    selectedOdor <- rownames(candi_A)[kNeighbors]
+    
+    # transpose matrix A
+    if (dim(A)[1] == 1) { transp_A <- t(t(c(as.matrix(A)))) } # transpose from 1 x m to m x 1 matrix
+    else { transp_A <- t(A) }
+    
+    alfa <- t(b) %*% PseudoInverse(transp_A) %*% as.matrix(w)
+    
+    result <- list(estimation = alfa,
+                   selected.receptors = selectReceptor,
+                   selected.odors = selectedOdor)
+    
+    return(result)
+  }
