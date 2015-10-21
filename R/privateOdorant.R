b02985c4e3fd5fdb797b9cba0ea49a2d2e5ad973
Dahaniel <daniel@muench.bio>
2015-10-21 11:23:33 +0200

    typos

--- R/privateOdorant.R
+++ R/privateOdorant.R
@@ -12,7 +12,7 @@
 #'
 #' @return a data.frame containing odorants and the response in the receptor of interest as well as the maximum response of the remaining receptors and their difference
 #' @export
-#' @importFrom stats na.omits
+#' @importFrom stats na.omit
 #'
 #' @examples
 #' library(DoOR.data)
