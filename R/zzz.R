ea5266b729b8b8a4c80102df20e9b8d47713ebdf
Daniel Münch <daniel@muench.bio>
2016-02-22 12:38:05 +0100

    removed message as DOoR.data is in dependencies

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0("\nWelcome to ", utils::packageDescription(pkgname)$Package,
                                "\nVersion: "  , utils::packageDescription(pkgname)$Version,
-                               "\nReleased: " , utils::packageDescription(pkgname)$Date,
-                               "\n\nRemember to also load DoOR.data.\n"
-  ))
+                               "\nReleased: " , utils::packageDescription(pkgname)$Date
+                               ))
 }
