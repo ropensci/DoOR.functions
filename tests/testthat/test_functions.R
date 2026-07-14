89afbf9b333fb4879aedf00edab4c6b291a34267
Dahaniel <daniel@muench.bio>
2026-07-14 13:14:25 +0200

    updated tests to use modern syntax -> fixes #19

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -6,16 +6,15 @@ context("correct output")
 test_that("reset_sfr produces data with SFR value of 0", {
   door_response_matrix_SFRreset <-
     reset_sfr(door_response_matrix, "SFR")
-  expect_that(all(door_response_matrix_SFRreset["SFR", ] == 0, na.rm = TRUE),
-              is_true())
+  expect_true(all(door_response_matrix_SFRreset["SFR", ] == 0, na.rm = TRUE))
 })
 
 test_that("door_norm rescales [0,1]", {
-  expect_that(range(door_norm(123:321)), is_identical_to(range(0, 1)))
+  expect_identical(range(door_norm(123:321)), range(0, 1))
 })
 
 test_that("calculate_model picks proper models", {
-  expect_that(names(calculate_model(
+  expect_match(names(calculate_model(
     x = door_norm(1:10), y = door_norm(1:10)
-  )), matches("linear"))
+  )), "linear")
 })
